ff78c04e7648536582cf5a0acf0f5af0e14372de
pratik.joshi <pratsj9@live.in>
2025-12-03 14:55:19 +0530

    Minor updates : description field on the Entity class

--- db/model/BookEntity.py
+++ db/model/BookEntity.py
@@ -3,9 +3,9 @@ from sqlmodel import SQLModel, Field
 
 class BookEntity(SQLModel,table=True):
     id: int = Field(primary_key=True , index=True)
-    title: str = Field()
-    author: str = Field()
-    year: int | None = Field(nullable=False)
-    isbn: str | None = Field(index=True , nullable=False)
-    available: bool = Field(default=True , nullable=False)
+    title: str = Field(description="The title of the Book")
+    author: str = Field(description="The author of the Book")
+    year: int | None = Field(nullable=False , description="Launch Year of this edition")
+    isbn: str | None = Field(index=True , nullable=False, description="ISBN number")
+    available: bool = Field(default=True , nullable=False , description="Is the book currently available")
 
