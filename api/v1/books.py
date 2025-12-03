7a7800429c494c55091eab093c1a5a70cfd57ec7
pratik.joshi <pratsj9@live.in>
2025-12-03 14:47:12 +0530

    Minor code refinements

--- api/v1/books.py
+++ api/v1/books.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
-from db.db import get_db, engine
+from db.db import get_db
 
 from db.model.BookEntity import BookEntity
 
@@ -13,10 +13,10 @@ SessionDep = Annotated[Session, Depends(get_db)]
 
 @router.get("/v1/books",summary="Retrieve all the books",status_code=200,
             tags=["books"])
-async def get_all_books(db: SessionDep):
+async def get_all_books(db: SessionDep) -> list:
     """
     Return all the available books from the database
-    :return:
+    :return: list
     """
     print("Retrieving all books")
     return db.query(BookEntity).all()
@@ -27,11 +27,13 @@ async def get_all_books(db: SessionDep):
 async def get_book_by_id(id:int , db: SessionDep) -> BookEntity:
     """
     Return a book based on the given ID
-    :return:
+    :return: BookEntity
     """
     print(f"Retrieving Book details for given id : {id}")
     book = (db.query(BookEntity)
             .filter(BookEntity.id == id).first())
+    if not book:
+        raise HTTPException(404 , "Book ID not found")
     return book
 
 
@@ -42,7 +44,6 @@ async def create_book(book_data:BookEntity , db: SessionDep) -> BookEntity:
     Create a new Book Entry in the database
     :param db:
     :param book_data:
-    :param payload:
     :return: BookEntity
     """
     print(f"Creating a book entry for {book_data}")
@@ -56,41 +57,39 @@ async def create_book(book_data:BookEntity , db: SessionDep) -> BookEntity:
 
 @router.put("/v1/books/{id}" , summary="Update a Book entry based on the given Id",
             status_code=200 , responses={404 : {"description": "Given Book Id not found"}}, tags=["books"])
-async def update_book(id: int , book_entry: BookEntity) -> BookEntity:
+async def update_book(id: int , book_entry: BookEntity , db:SessionDep) -> dict:
     """
     Update a Book entry based on the given ID
-    :param book_data:
+    :param db:
+    :param book_entry:
     :param id:
-    :param payload:
     :return:
     """
     print(f"Updating the book entry for the given id {id}")
-    with Session(engine) as session:
-        book_session = session.get(BookEntity, id)
-        if not book_session:
-            raise HTTPException(status_code=404)
-        book_data = book_entry.model_dump(exclude_unset=True)
-        book_session.sqlmodel_update(book_data)
-        session.add(book_session)
-        session.commit()
-        return book_data
+    book_session = db.get(BookEntity , id)
+    if not book_session:
+        raise HTTPException(404,"Book ID not found")
+    book_data = book_entry.model_dump(exclude_unset=True)
+    book_session.sqlmodel_update(book_data)
+    db.commit()
+    return book_data
 
 
 @router.delete("/v1/books/{id}" , summary="Delete a Book entry based on the given Id",
                status_code=204 , responses={404 : {"description": "Given Book Id not found"}}, tags=["books"])
-async def delete_book(id: int):
+async def delete_book(id: int , db:SessionDep):
     """
     Delete a Book entry based on the given Id
+    :param db:
     :param id:
     :return:
     """
-    with Session(engine) as session:
-        book_session = session.get(BookEntity, id)
-        if not book_session:
-            raise HTTPException(status_code=404)
-        session.delete(book_session)
-        session.commit()
-        return {"Book Entry Deleted": "ok"}
+    book_entry = db.get(BookEntity, id)
+    if not book_entry:
+        raise HTTPException(404,"Book ID not found")
+    db.delete(book_entry)
+    db.commit()
+    return {"Book Entry Deleted": "ok"}
 
 
 
